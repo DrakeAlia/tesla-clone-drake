423587213ffe90f018189ac994ad9fadafa8cbbf
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2021-10-07 16:57:35 -0700

    inital setup for homepage w/ section

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,7 +4,20 @@ import styled from 'styled-components'
 function Section() {
     return (
         <Wrap>
-            
+            <ItemText>
+                <h1>Model S</h1>
+                <p>Order Online for Touchless Delivery</p>
+            </ItemText>
+            <ButtonGroup>
+                <LeftButton>
+                    Custom Order
+                </LeftButton>
+                <RightButton>
+                    Existing Inventory
+                </RightButton>
+            </ButtonGroup>
+
+
         </Wrap>
     )
 }
@@ -12,9 +25,37 @@ function Section() {
 export default Section
 
 const Wrap = styled.div`
-width: 100vw;
-height: 100vh;
-background-size: cover;
-background-position: center;
-background-image: url('/images/teslaModelS.png');
+    width: 100vw;
+    height: 100vh;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-image: url('/images/teslaModelS.png');
+`
+
+const ItemText = styled.div`
+    padding-top: 15vh;
+    text-align: center;
+`
+
+const ButtonGroup = styled.div`
+    display: flex;
+`
+
+const LeftButton = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px;
+`
+
+const RightButton = styled(LeftButton)`
+    
 `
