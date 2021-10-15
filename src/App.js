2d3a1d18ef37aea424d17c020bf6dad2e08d313a
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2021-10-15 16:53:57 -0700

    update v3

--- src/App.js
+++ src/App.js
@@ -4,32 +4,32 @@ import Header from './components/Header';
 import Home from './components/Home';
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
+const App = () => {
+  return (
+    // <div>
+    <Router>
+     <div className="App">
+    <Header />
+    {/* <Switch> */}
+     {/* <Route exact path='/'>  */}
+    <Home />
+     {/* </Route> */}
+     {/* <Route path='/models' component={} />
+     <Route path='/solarroof' component={} />
+     <Route path='/solarpanels' component={} /> */}
+    {/* </Switch> */}
+    </div>
+    </Router>
+  );
+}
+
 // const App = () => {
 //   return (
-//     <div>
-//     <Router>
-//     <Header />
-//     <Switch>
-//      <Route exact path='/'> 
-//     <Home />
-//      </Route>
-//      <Route path='/models' component={} />
-//      <Route path='/solarroof' component={} />
-//      <Route path='/solarpanels' component={} />
 //     <div className="App">
-//     </Switch>
-//     </Router>
+//     <Header />
+//     <Home />
 //     </div>
 //   );
 // }
-
-const App = () => {
-  return (
-    <div className="App">
-    <Header />
-    <Home />
-    </div>
-  );
-}
 
 export default App;
