675a59d9e5d7aa3a09ebbad4927a4f9068494abc
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2021-11-24 15:07:01 -0800

    added BtnGroup to one of feature buttons

--- src/components/designs/ModelSDesign.js
+++ src/components/designs/ModelSDesign.js
@@ -1,415 +1,3 @@
-// import styled from 'styled-components'
-// import { useEffect } from 'react';
-// // import { useState } from 'react';
-// import ExpandLessIcon from '@mui/icons-material/ExpandLess';
-// import Logo from '../Logo';
-
-
-// const ModelSDesign = () => {
-//     useEffect(() => {
-//         console.log('Im Here!!!')
-//     }, []);
-
-// // const [details, setDestails] = useState(null)
-// // const [price, setPrice] = useState(null)
-// // const [allWheelDrive, setAllWheelDrive] = useState(null)
-// // const [paint, setPaint] = useState(null)
-// // const [wheels, setWheels] = useState(null)
-// // const [interior, setInterior] = useState(null)
-
-//     return (
-//         <Wrap>
-//             <Logo />
-//             <SideBar>
-//                 <Container>
-//                     <Title>Model S</Title>
-//                 </Container>
-
-//                 <DeliveryTime>Est. Devilvery: June</DeliveryTime>
-
-//                 <Container>
-//                     <Box>
-//                         <BtnGroup>
-//                             <Item>Purchase Price</Item>
-//                             <Item>Potential Savings*</Item>
-//                         </BtnGroup>
-//                     </Box>
-//                 </Container>
-
-//                 <Container>
-//                     <Box>
-//                         <Range>
-//                             <Stats>375
-//                             <Stats2>mi</Stats2>
-//                             </Stats>
-//                             <Stats3>Range (est.)</Stats3>
-//                         </Range>
-//                         <Range>
-//                             <Stats>155
-//                             <Stats2>mph</Stats2>
-//                             </Stats>
-//                             <Stats3>Top Speed</Stats3>
-//                         </Range>
-//                         <Range>
-//                             <Stats>3.1
-//                             <Stats2>sec</Stats2>
-//                             </Stats>
-//                             <Stats3>0-60mph</Stats3>
-//                         </Range>
-//                     </Box>
-//                 </Container>
-
-//                 <Container>
-//                     <Name>Dual Motor All-Wheel Drive</Name>
-//                     <Box>
-//                         <BtnGroup>
-//                             <Item2>Model S</Item2>
-//                             <Item2>$89,490*</Item2>
-//                         </BtnGroup>
-//                     </Box>
-//                 </Container>
-
-//                 <Container>
-//                     <Name>Tri Motor All-Wheel drivers</Name>
-//                     <Box>
-//                         <BtnGroup>
-//                             <Item2>Model S Plaid</Item2>
-//                             <Item2>$124,490*</Item2>
-//                         </BtnGroup>
-//                     </Box>
-//                 </Container>
-
-//                 <Container>
-//                     <Box>
-//                         {/* <Desc>The Prices above include potential incentives
-//                             and gas savings of $5,500.
-//                             <p>Learn More</p></Desc> */}
-
-//                         <Desc>The Prices above include potential incentives
-//                             and gas savings of $5,500.
-//                             <Learn>Learn More</Learn>
-//                             </Desc>
-//                     </Box>
-//                 </Container>
-
-//                 <Container>
-//                     <Box>
-//                         <BtnGroup>
-//                             <Feature>Feature Details</Feature>
-//                         </BtnGroup>
-//                     </Box>
-//                 </Container>
-
-
-
-
-//                 <Container>
-//                     <Title>Paint</Title>
-//                     <Selector></Selector>
-//                     <Desc>Pearl White Multi-Coat Included</Desc>
-//                 </Container>
-
-//                 <Container>
-//                     <Title>Wheels</Title>
-//                     <Selector></Selector>
-//                     <Desc>21" Arachnid Wheels</Desc>
-//                     <Desc>$4,500</Desc>
-//                     <Desc>Range (est.): 375mi</Desc>
-//                 </Container>
-
-//                 <Container>
-//                     <Title>Interior</Title>
-//                     <Selector></Selector>
-//                     <Desc>All Black Included</Desc>
-//                     <Desc>Ebony Décor</Desc>
-//                 </Container>
-
-//                 <Container>
-//                     <Box>
-//                         <Feature>Feature Details</Feature>
-//                     </Box>
-//                 </Container>
-
-//                 <Container>
-//                     <Title>Full Self-Driving Capability</Title>
-//                     <Desc>$10,000</Desc>
-//                     <Details>
-
-//                         <li>Navigate on Autopilot</li>
-//                         <li>Auto Lane Change</li>
-//                         <li>Autopark</li>
-//                         <li>Summon</li>
-//                         <li>Full Self-Driving Computer</li>
-//                         <li>Traffic Light and Stop Sign Control</li>
-
-//                         <Desc>Coming Soon</Desc>
-//                         <li>Autosteer on city streets</li>
-//                     </Details>
-//                     <Desc>The currently enabled features require active driver
-//                         supervision and do not make the vehicle autonomous. The activation and use of these features
-//                         are dependent on achieving reliability far in excess of human drivers as demonstrated by
-//                         billions of miles of experience, as well as regulatory approval, which may take longer in
-//                         some jurisdictions. As these self-driving features evolve, your car will be continuously
-//                         upgraded through over-the-air software updates.
-//                     </Desc>
-//                 </Container>
-
-//                 <Container>
-//                     <Box>
-//                         <BtnBox>
-//                             <Btn>Add</Btn>
-//                         </BtnBox>
-//                     </Box>
-//                     <Box>
-//                         <BtnGroup>
-//                             <Feature>Feature Details</Feature>
-//                         </BtnGroup>
-//                     </Box>
-//                 </Container>
-
-//                 <Container>
-//                     <Title>Order Your Model S</Title>
-//                     <DeliveryTime>Est. Delivery: June</DeliveryTime>
-//                     <BtnBox>
-//                         <Btn>Continue Payment</Btn>
-//                     </BtnBox>
-//                 </Container>
-
-//             </SideBar>
-
-//             <Footer>
-//                 <FooterBox>
-//                     <FooterBtn>
-//                         <ExpandLessIcon></ExpandLessIcon>
-//                         {/* <UpArrow src="/images/Up-Arrow.svg" /> */}
-//                     </FooterBtn>
-//                     <FooterItem>$100,990 Purchase price</FooterItem>
-//                     <FooterItem>$95,490 After potential savings</FooterItem>
-//                 </FooterBox>
-//             </Footer>
-//         </Wrap>
-//     )
-// }
-
-// export default ModelSDesign
-
-
-// // Container for the whole page
-// const Wrap = styled.div`
-//     display: flex; 
-//     flex-direction: column;
-//     justify-content: center;
-// `
-
-// // SideBar Container for the model details
-// const SideBar = styled.div`
-//     display: flex;
-//     flex-direction: column;
-//     justify-content: center;
-//     align-self: flex-end;
-//     overflow-y: scroll;
-//     padding: 24px;
-//     width: 600px;
-//     @media (max-width: 823px) {
-//         flex-direction: column;
-//     }
-// `
-
-// // Model name/title
-// const Title = styled.div`
-//     display: flex;
-//     justify-content: center;
-//     padding-top: 25px;
-//     font-size: 40px;
-//     color: #393c41;
-// `
-
-// // Est. Devilvery: June
-// const DeliveryTime = styled.div`
-//     display: flex;
-//     justify-content: center;
-//     color: #393c41;
-//     padding: 24px;
-//     padding-top: 5px;
-//     padding-bottom: 10px;
-// `
-
-// // 
-// const Container = styled.div`
-//     display: flex;
-//     flex-direction: column;
-//     align-content: center;
-//     padding: 20px;
-//     padding-bottom: 10px;
-// `
-
-// // 
-// const Box = styled.div`
-//     display: flex;
-//     flex-direction: row;
-//     justify-content: center;
-//     padding: 20px;
-// `
-
-// const Range = styled(Box)`
-//     display: flex;
-//     flex-direction: column;
-// `
-
-// // 
-// const Stats = styled.div`
-//     display: flex;
-//     justify-content: center;
-//     color: #393c41;
-// `
-
-// // 
-// const Stats2 = styled(Stats)`
-// `
-
-// // 
-// const Stats3 = styled(Stats)`
-// `
-
-// // 
-// const BtnGroup = styled.div`
-//     display: flex;
-//     flex-direction: column;
-//     padding-left: 30px;
-//     padding-right: 30px;
-// `
-
-// // 
-// const Details = styled.div`
-//     display: flex;
-//     flex-direction: column;
-//     justify-content: space-between;
-//     padding: 24px;
-//     color: #393c41;
-// `
-
-// // 
-// const Item = styled.div`
-//     display: flex;
-//     color: #393c41;
-// `
-
-// const Item2 = styled(Item)`
-
-// `
-
-// const Item3 = styled(Item)`
-//     display: flex;
-//     color: #393c41;
-// `
-
-
-// // Name of the either Dual or Tri motor
-// const Name = styled.div`
-//     display: flex;
-//     justify-content: flex-start;
-//     color: #393c41;
-// `
-
-// // Description
-// const Desc = styled.div`
-//     display: flex;
-//     flex-direction: column;
-//     align-items: center;
-//     font-size: 15px;
-//     color: #393c41;
-//     p {
-//         display: flex;
-//         text-decoration: underline; 
-//     }
-//     @media (max-width: 823px) {
-//         flex-direction: column;
-//     }
-// `
-
-// // Description
-// const Learn = styled(Desc)`
-//     display: flex;
-//     flex-direction: column;
-//     align-items: center;
-//     font-size: 15px;
-//     color: #393c41;
-// `
-
-
-// // Choose wheels/paint/ect. options
-// const Selector = styled.div`
-//     display: flex;
-//     justify-content: center;
-//     color: #393c41;
-// `
-
-// // 
-// const BtnBox = styled.div`
-//     display: flex;
-//     justify-content: center;
-//     padding: 24px;
-// `
-
-// // Button
-// const Btn = styled.div`
-//     display: flex;
-//     background: #0000FF;
-//     color: #ffffff;
-//     text-transform: uppercase;
-//     padding: 10px;
-//     border-radius: 100px;
-//     cursor: pointer;
-// `
-
-// // Feature details container
-// const Feature = styled.div`
-//     display: flex;
-//     background: #D3D3D3;
-//     color: #393c41;
-//     text-transform: uppercase;
-//     padding: 10px;
-//     border-radius: 100px;
-//     cursor: pointer;
-// `
-
-
-// // Bottom container 
-// const Footer = styled.div`
-//     display: flex;
-//     justify-content: center;
-//     align-content: center;
-//     padding-top: 2px;
-//     @media (max-width: 800px) {
-//         flex-direction: row;
-//     }
-// `
-
-// // Bottom container 
-// const FooterBox = styled.div`
-//     display: flex;
-//     justify-content: center;
-// `
-
-// // Footer button arrow
-// const FooterBtn = styled.div`
-//     display: flex;
-//     padding-top: 2px;
-//     padding: 10px;
-//     cursor: pointer;
-//     color: #393c41;
-// `
-
-// // Footer items in container
-// const FooterItem = styled.div`
-//     padding-top: 2px;
-//     padding: 10px;
-//     cursor: pointer;
-//     color: #393c41;
-// `
-
-
-
 import styled from 'styled-components'
 import { useEffect } from 'react';
 // import { useState } from 'react';
@@ -472,12 +60,12 @@ const ModelSDesign = () => {
                     <Name>Dual Motor All-Wheel Drive</Name>
                     <Box4>
                         <ModelGroup>
-                        <ModelBox>
-                            <Item>Model S</Item>
-                        </ModelBox>
-                        <ModelBox>
-                            <Item>$89,490*</Item>
-                        </ModelBox>
+                            <ModelBox>
+                                <Item>Model S</Item>
+                            </ModelBox>
+                            <ModelBox>
+                                <Item>$89,490*</Item>
+                            </ModelBox>
                         </ModelGroup>
                     </Box4>
                 </Container>
@@ -486,22 +74,22 @@ const ModelSDesign = () => {
                     <Name>Tri Motor All-Wheel drivers</Name>
                     <Box4>
                         <ModelGroup>
-                        <ModelBox>
-                            <Item>Model S Plaid</Item>
-                        </ModelBox>
-                        <ModelBox>
-                            <Item>$124,490*</Item>
-                        </ModelBox>
+                            <ModelBox>
+                                <Item>Model S Plaid</Item>
+                            </ModelBox>
+                            <ModelBox>
+                                <Item>$124,490*</Item>
+                            </ModelBox>
                         </ModelGroup>
                     </Box4>
                 </Container>
 
                 <Container>
                     <Box5>
-                        <Desc>* Prices above include potential incentives and </Desc>
-                        <Desc>gas savings of $5,500.</Desc>
+                        <Desc>* Prices above include potential incentives and
+                        gas savings of $5,500.</Desc>
                         <Learn>Learn More</Learn>
-                        
+
                     </Box5>
                 </Container>
 
@@ -513,16 +101,14 @@ const ModelSDesign = () => {
                     </Box>
                 </Container>
 
-
-
                 <Container>
-                    <Title>Paint</Title>
+                    <SubTitle>Paint</SubTitle>
                     <Selector></Selector>
                     <Desc>Pearl White Multi-Coat Included</Desc>
                 </Container>
 
                 <Container>
-                    <Title>Wheels</Title>
+                    <SubTitle>Wheels</SubTitle>
                     <Selector></Selector>
                     <Desc>21" Arachnid Wheels</Desc>
                     <Desc>$4,500</Desc>
@@ -530,7 +116,7 @@ const ModelSDesign = () => {
                 </Container>
 
                 <Container>
-                    <Title>Interior</Title>
+                    <SubTitle>Interior</SubTitle>
                     <Selector></Selector>
                     <Desc>All Black Included</Desc>
                     <Desc>Ebony Décor</Desc>
@@ -538,22 +124,22 @@ const ModelSDesign = () => {
 
                 <Container>
                     <Box>
-                        <Feature>Feature Details</Feature>
+                        <BtnGroup>
+                            <Feature>Feature Details</Feature>
+                        </BtnGroup>
                     </Box>
                 </Container>
 
                 <Container>
-                    <Title>Full Self-Driving Capability</Title>
+                    <SubTitle>Full Self-Driving Capability</SubTitle>
                     <Desc>$10,000</Desc>
                     <Details>
-
                         <li>Navigate on Autopilot</li>
                         <li>Auto Lane Change</li>
                         <li>Autopark</li>
                         <li>Summon</li>
                         <li>Full Self-Driving Computer</li>
                         <li>Traffic Light and Stop Sign Control</li>
-
                         <Desc>Coming Soon</Desc>
                         <li>Autosteer on city streets</li>
                     </Details>
@@ -638,6 +224,10 @@ const Title = styled.div`
     font-size: 40px;
 `
 
+const SubTitle = styled(Title)`
+    font-size: 25px;
+`
+
 // Est. Devilvery: June
 const DeliveryTime = styled.div`
     display: flex;
@@ -719,7 +309,7 @@ const Stats3 = styled(Stats)`
 // 
 const BtnGroup = styled.div`
     display: flex;
-    flex-direction: column;
+    padding: 10px;
     /* padding-left: 30px;
     padding-right: 30px; */
 `
@@ -809,6 +399,7 @@ const Btn = styled.div`
 // Feature details container
 const Feature = styled.div`
     display: flex;
+    justify-content: center;
     background: #D3D3D3;
     color: #393c41;
     border-radius: 100px;
